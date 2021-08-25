a53d4bc538c5cd8a66e4b4f8e34510e020010ecf
Mohamed Farook Mohamed Fazrin <mohamed.farzin@idsnext.com>
2021-08-26 00:11:45 +0530

    GetAllCompanyWithEmployees

--- DapperDemo/Migrations/20210825183143_init.Designer.cs
+++ DapperDemo/Migrations/20210825183143_init.Designer.cs
@@ -3,14 +3,16 @@ using DapperDemo.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DapperDemo.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210825183143_init")]
+    partial class init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
